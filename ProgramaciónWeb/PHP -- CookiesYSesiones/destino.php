4b804b83e29c3d880491a0700100c84107feced3
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-02-22 14:31:17 -0300

    nuevo código

---
+++ ProgramaciónWeb/PHP -- CookiesYSesiones/destino.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+
+    $nombreUsuario="";
+    $estado="";
+
+    if ( isset( $_SESSION['nombreUsuario'] ) ) {
+        //session_name( session_id() );
+        $nombreUsuario = $_SESSION['nombreUsuario'];
+        $estado = "Se recuperó una sesión existente a nombre de: $nombreUsuario";
+    }
+    else {
+        $_SESSION['nombreUsuario'] = $_POST['nombreUsuario'];
+        session_name( $_SESSION['nombreUsuario'] );
+        $nombreUsuario = $_SESSION['nombreUsuario'];
+        $estado = "Se inició una sesión a nombre de: $nombreUsuario";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesiones</title>
+</head>
+<body>
+
+    <b>
+        <?php
+        echo $estado;
+        ?>
+    </b>
+
+    <p>
+        <?php
+            echo 'ID de sesión: <b>'.session_id().'</b> '.session_name();
+        ?>
+    </p>
+
+    <p>
+        <a href="cerrarSesion.php">
+            <button>Cerrar Sesión</button>
+        </a>
+    </p>
+
+    <?php
+        echo ini_get("session.use_cookies");
+    ?>
+</body>
+</html>
---
+++ ProgramaciónWeb/PHP -- CookiesYSesiones/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies y Sesiones en PHP</title>
+</head>
+<body>
+    <form action="destino.php" id="formulario" method="post">
+        <label for="nombreUsuario">Nombre:</label>
+        <input type="text" name="nombreUsuario" id="nombreUsuario">
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
