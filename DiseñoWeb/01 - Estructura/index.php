be0a2c14fb638be1b2bb98cf07a1b05d2ca19c00
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-03-25 00:15:00 -0300

    Actualización del código

---
+++ DiseñoWeb/01 - Estructura/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- 
+        En este primer acercamiento al manejo de una estructura de página web
+        usaremos la siguietne maqueta: 
+
+ -->
+
+
+
+    <div id="banner">
+        <div id="ticker"></div>
+    </div>
+    <div id="menu"></div>
+    <div id="contenido"></div>
+    
+</body>
+</html>
