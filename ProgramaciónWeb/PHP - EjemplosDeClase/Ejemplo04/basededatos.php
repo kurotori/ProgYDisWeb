e4b6c5c403b048c7354429c6cc279d99bc5da536
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-06-03 11:19:07 -0300

    Nuevo código

---
+++ ProgramaciónWeb/PHP - EjemplosDeClase/Ejemplo04/basededatos.php
@@ -0,0 +1,46 @@
+<?php
+
+    function CrearConexion(){
+        $servidor = "localhost";
+        $usuario = "root";
+        $password = "soloyoeh";
+        $bdd = "libreria";
+
+        $conexion = new mysqli($servidor,$usuario,$password,$bdd);
+
+        if( $conexion->connect_error ){
+            die("ERROR: " . $conexion->connect_error);
+        }
+
+        return $conexion;
+    }
+
+
+
+    function ObtenerLibrosEnTabla(){
+        $conexion = CrearConexion();
+
+        $consulta = "SELECT titulo,genero,fecha_pub,ISBN from libro order by genero,titulo";
+
+        $resultado = $conexion->query($consulta);
+
+        $tabla = "";
+
+        if( $resultado->num_rows > 0 ){
+
+            while ( $fila = $resultado->fetch_assoc() ) {
+                $tabla = $tabla . "<tr>";
+                $tabla = $tabla . "<td>" . $fila['titulo'] . "</td>";
+                $tabla = $tabla . "<td>" . $fila['genero'] . "</td>";
+                $tabla = $tabla . "<td>" . $fila['fecha_pub'] . "</td>";
+                $tabla = $tabla . "<td>" . $fila['ISBN'] . "</td>";
+                $tabla = $tabla . "</tr>";
+            }
+
+        }
+        else{
+            $tabla = "<tr><td colspan=4> No se encontraron libros </td></tr>";
+        }
+    }
+
+?>
---
+++ ProgramaciónWeb/PHP - EjemplosDeClase/Ejemplo04/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librería</title>
+</head>
+<body>
+
+    <h1>Listado Completo de Libros</h1>
+
+    <table border=1>
+        <tr>
+            <th>Título</th>
+            <th>Género</th>
+            <th>Fecha de Publicación</th>
+            <th>ISBN</th>
+        </tr>
+        <tr>
+            <td>algo</td>
+            <td>algo</td>
+            <td>algo</td>
+            <td>algo</td>
+        </tr>
+    </table>
+    
+</body>
+</html>
