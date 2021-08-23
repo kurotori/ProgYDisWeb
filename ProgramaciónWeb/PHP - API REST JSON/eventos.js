5acac4ab4cfbafb895fbbba89771855324bbd8ee
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-08-23 18:58:12 -0300

    Código actualizado

---
+++ ProgramaciónWeb/PHP - API REST JSON/eventos.js
@@ -0,0 +1,41 @@
+function EnviarFormulario(nombreUsuario, hashPass) {
+    
+    $.ajax(
+        {
+            //1 - Indicar la URL de donde se obtienen los datos
+            url:"registro.php",
+            //2 - Método para el envío de los datos, puede ser 'GET' o 'POST'
+            method: "POST",
+            //3 - Indicar la forma que tendran los datos, en este caso es 'json'
+            datatype: "json",
+            //4 - Indicar los datos que se incluirán. 
+            // Primero se indica el nombre del dato esperado por la página y luego el dato
+            data:{
+                nombre : nombreUsuario,
+                hash : hashPass
+            },
+            //5 - Establecemos una función que se ejecuta en caso de éxito en la operación
+            success:function (data) {
+                //la variable 'data' representa a los datos que vienen del servidor
+                alert("Los datos se recibieron en el servidor");
+            },
+            //6 - Establecemos una función que se ejecuta en caso de error
+            error:function(errorThrown){
+                console.error(errorThrown.responseText);
+            }
+        }
+    );
+
+    
+}
+
+function GenerarHash(password) {
+        //1 - Creamos un objeto de hasheo
+        var hasheador = new jsSHA("SHA-512", "TEXT", {numRounds: 1});
+        //2 - Agregamos el password al objeto de hasheo
+        hasheador.update(password);
+        //3 - Obtenemos el hash en formato hexadecimal...
+        var hash = hasheador.getHash("HEX");
+        // ...y devolvemos el mismo.
+        return hash;
+}
--- ProgramaciónWeb/PHP - API REST JSON/index.php
+++ ProgramaciónWeb/PHP - API REST JSON/index.php
@@ -2,9 +2,26 @@
     session_start();
     include "funciones.php";
 
-    $token = GenerarToken();
-    echo($token);
-    $verif = password_verify(,$token);
-    echo("<br>");
-    echo($verif);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="jquery-3.6.0.js"></script>
+    <script src="sha512.js"></script>
+    <script src="eventos.js"></script>
+</head>
+<body>
+    <form action="registro/" method="post">
+        <label for="nombre"></label>
+        <input type="text" name="nombre" id="nombre">
+        <label for="pass"></label>
+        <input type="password" name="pass" id="pass">
+        <button>Registrarse</button>
+    </form>
+</body>
+</html>
