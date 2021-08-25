73a8845fbd874e3e2554d49adf197248cde93954
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-08-24 21:21:49 -0300

    Nuevo código: validación de formularios

--- DisWeb2/Introducción a JavaScript/JavaScript 06 - Validar Formularios/eventos.js
+++ DisWeb2/Introducción a JavaScript/JavaScript 06 - Validar Formularios/eventos.js
@@ -2,8 +2,7 @@
  * Función para comprobar que la longitud del contenido de un campo no sea 0.
  * Si un campo no tiene contenido, devuelve 'false'
  */
-function ValidarCampo(idCampo) {
-    var campo = "#" + idCampo;
+function ValidarCampo(campo) {
     
     if ( $(campo).val().length == 0 ) {
         return false;
@@ -15,14 +14,32 @@ function ValidarCampo(idCampo) {
 function ValidarFormulario(idFormulario) {
     var formulario = "#" + idFormulario;
 
-    var campos = $(formulario + "> :input");
+    //Obtengo todos los elementos del formulario
+    var campos = $(formulario + ">*");
 
+    //Establezco un bucle que recorre todos los elementos
+    // del formulario mediante la función .each()
     campos.each(
+        //Establecemos una función anónima para trabajar sobre los elementos
+        // del formulario.
        function(){
-            if ( $(this).val().length == 0 ) {
-                alert( "El campo "+ this.id + " esta vacío");
-                return;
+            
+            //Filtramos los elementos según su tipo de etiqueta con la función
+            // .prop('tagName'), y elegimos solo los de tipo 'INPUT'
+            if( $(this).prop("tagName") == "INPUT" ){
+                
+                //Si la validación devuelve 'false' le agregamos a los campos
+                // sin contenido la clase 'error' con la función .addClass()
+                if ( ! ValidarCampo(this) ) {
+                    $(this).addClass("error");
+                    
+                    //Código anterior: muestra una advertencia por cada campo.
+                    //alert( "El campo "+ this.id + " esta vacío");
+                    //return false;
+                }
             }
+
+            
        }
    );
 
