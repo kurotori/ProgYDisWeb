06da53d3b96c3e7b14d24131478f595bf85ddaf1
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-10-08 18:44:08 -0300

    Actualización

--- EjemplosUtiles/RegistroConFoto/eventos.js
+++ EjemplosUtiles/RegistroConFoto/eventos.js
@@ -4,12 +4,21 @@ function CargarImagenes() {
 
     inputArchivos.forEach(imagen => {
         imgNuevaImagen = document.createElement('img');
-        let lector = new FileReader();
-        lector.readAsDataURL(imagen)
-        lector.onload = function (datos) {
-            imgNuevaImagen.src = datos.target.result;
-        }
+        srcImagen = LeerDatosImagen(imagen);
+        imgNuevaImagen.src = srcImagen;
         imgNuevaImagen.style.width="200px";
         divVistaPrevia.appendChild(imgNuevaImagen);
     });
 }
+
+function LeerDatosImagen(archivoImagen) {
+    let lector = new FileReader();
+    lector.readAsDataURL(archivoImagen);
+    var datos;
+    lector.onload = function (data) {
+        datos = data.target.result;
+    }
+    return datos;
+}
+
+//https://web.dev/read-files/
