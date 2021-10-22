ddd92f799edfe6099ad4495eab5cbc0488dc320e
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-10-22 13:12:36 -0300

    Actualizacón y nuevo código

--- EjemplosUtiles/EjemploAPI/eventos.js
+++ EjemplosUtiles/EjemploAPI/eventos.js
@@ -22,6 +22,77 @@ function mostrarBusqueda() {
     divBusquedaBuscador.style.display = "block";
 }
 
+/**
+ * Realiza una consulta de búsqueda de libros
+ * y organiza el resultado utilizando una tabla.
+ */
+function buscarLibrosATabla() {
+    //Vaciamos el contenido del div donde mostraremos el resultado.
+    divBusquedaContenido.innerHTML = "";
+
+    let valorBusqueda = inputBuscador.value;
+    $.ajax(
+        {
+            //1 - Indicar la URL de donde se obtienen los datos
+            url:"api.php",
+            //2 - Método para el envío de los datos, puede ser 'GET' o 'POST'
+            method: "POST",
+            //3 - Indicar la forma que tendran los datos, en este caso es 'json'
+            datatype: "json",
+            //4 - Indicar los datos que se incluirán. 
+            // Primero se indica el nombre del dato esperado por la página y luego el dato
+            data:{
+                'modo' : 2,
+                'busqueda' : valorBusqueda
+            },
+            //5 - Establecemos una función que se ejecuta en caso de éxito en la operación
+            success:function (datos) {
+                
+                console.log(datos);
+            
+                if (datos.Respuesta.estado == "OK") {
+                    listado = datos.Respuesta.datos;
+
+                    listado.forEach(libro => {
+                        const pLibro =  document.createElement('p');
+                        const bTitulo = document.createElement('b');
+                        const aLinkLibro = document.createElement('a');
+                        const iGenero = document.createElement('i');
+                        const iFechaPub = document.createElement('i');
+                        
+                        bTitulo.textContent = libro.titulo;
+                        aLinkLibro.href = "verlibro.html?isbn="+libro.isbn;
+                        aLinkLibro.appendChild(bTitulo);
+
+                        iGenero.textContent = " "+libro.genero;
+                        iFechaPub.textContent = " - "+libro.fecha_pub;
+    
+                        pLibro.appendChild(aLinkLibro);
+                        pLibro.appendChild(iGenero);
+                        pLibro.appendChild(iFechaPub);
+                        divBusquedaContenido.appendChild(pLibro);
+                    });
+                }
+                else{
+                    const pAviso =  document.createElement('p');
+                    pAviso.textContent = datos.Respuesta.estado + " : "+datos.Respuesta.datos;
+                    divBusquedaContenido.appendChild(pAviso);
+                }
+                //la variable 'datos' representa a los datos que vienen del servidor
+            },
+            //6 - Establecemos una función que se ejecuta en caso de error
+            error:function(errorThrown){
+                console.error("ERROR:" + errorThrown.responseText);
+            }
+        }
+    );
+}
+
+
+/**
+ * Realiza una consulta de búsqueda de libros
+ * y organiza el resultado utilizando elementos 'p'
+ */
 function buscarLibros() {
     divBusquedaContenido.innerHTML = "";
     let valorBusqueda = inputBuscador.value;
