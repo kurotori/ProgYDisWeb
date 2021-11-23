38900b210bde25c9ad0c7b7b990bff42f0a23778
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-11-22 21:55:09 -0300

    Nuevo código

---
+++ EjemplosUtiles/OtrasCosas/pruebas.js
@@ -0,0 +1,26 @@
+
+
+
+function ObtenerVecinos(celda) {
+    
+}
+
+
+
+class Celda{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.vecinos = Array();
+    }
+
+}
+
+let tablero = Array();
+
+function CompletarTAblero(params) {
+    
+}
+
+
+const c = new Celda(1,1);
