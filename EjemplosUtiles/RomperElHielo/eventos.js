c5c043eb6384055d862bafefe9f415ebe304cf8d
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-11-23 18:30:18 -0300

    Modificaciones

--- EjemplosUtiles/RomperElHielo/eventos.js
+++ EjemplosUtiles/RomperElHielo/eventos.js
@@ -1,6 +1,7 @@
+const divBloque1 = document.getElementById('bloque1');
+
 document.addEventListener('click', function() {
-    
-    //console.log(window.event.target);
+
     let objeto = window.event.target;
     
     let objClases = Array.from(objeto.classList);
@@ -8,53 +9,60 @@ document.addEventListener('click', function() {
     let posRatonX = window.event.clientX;
     let posRatonY = window.event.clientY;
 
-    if (objClases.includes('bloque')) {
-        console.log(objeto);
-        let objAncho = objeto.clientWidth;
-        let objAltura = objeto.clientHeight;
-
-        let posObjetoX = objeto.getBoundingClientRect().left;
-        let posObjetoY = objeto.getBoundingClientRect().top;
-
-        console.log(posObjetoX + " " + posObjetoY);
-
-        const bloque1 = document.createElement('div');
-        const bloque2 = document.createElement('div');
-        bloque1.classList.add('bloque','hielo');
-        bloque2.classList.add('bloque','hielo');
-
-        if (objAncho > objAltura) {
-            let porcX1 = ((posRatonX - posObjetoX)*100/objAncho);
-            let porcX2 = 100-porcX1;
-            bloque1.style.width = "calc("+porcX1+'% - 4px)';
-            bloque2.style.width = "calc("+porcX2+'% - 4px)';
-
-            bloque1.classList.add('alto');
-            bloque2.classList.add('alto');
-        } 
-        else {
-            let porcX1 = ((posRatonY - posObjetoY)*100/objAltura);
-            let porcX2 = 100-porcX1;
-            bloque1.style.height = "calc("+porcX1+'% - 4px)';
-            bloque2.style.height = "calc("+porcX2+'% - 4px)';
-
-            bloque1.classList.add('ancho');
-            bloque2.classList.add('ancho');
+    if (objeto.childElementCount < 2) {
+        if (objClases.includes('bloque')) {
+            let objAncho = objeto.clientWidth;
+            let objAltura = objeto.clientHeight;
+    
+            let posObjetoX = objeto.getBoundingClientRect().left;
+            let posObjetoY = objeto.getBoundingClientRect().top;
+    
+    
+            const bloque1 = document.createElement('div');
+            const bloque2 = document.createElement('div');
+            bloque1.classList.add('bloque','hielo');
+            bloque2.classList.add('bloque','hielo');
+    
+            if (objAncho > objAltura) {
+                let porcX1 = ((posRatonX - posObjetoX)*100/objAncho);
+                let porcX2 = 100-porcX1;
+                bloque1.style.width = "calc("+porcX1+'% - 4px)';
+                bloque2.style.width = "calc("+porcX2+'% - 4px)';
+    
+                bloque1.classList.add('alto');
+                bloque2.classList.add('alto');
+            } 
+            else {
+                let porcX1 = ((posRatonY - posObjetoY)*100/objAltura);
+                let porcX2 = 100-porcX1;
+                bloque1.style.height = "calc("+porcX1+'% - 4px)';
+                bloque2.style.height = "calc("+porcX2+'% - 4px)';
+    
+                bloque1.classList.add('ancho');
+                bloque2.classList.add('ancho');
+            }
+    
+            objeto.appendChild(bloque1);
+            objeto.appendChild(bloque2);
+            objeto.classList.remove('hielo');
+            objeto.classList.add('agua');
+    
         }
-
-        objeto.appendChild(bloque1);
-        objeto.appendChild(bloque2);
-        objeto.classList.remove('hielo');
-        objeto.classList.add('agua');
-
     }
+
+    
 
 }, false);
 
-/*document.addEventListener('pointermove',
-    function(){
-        let posRatonX = window.event.clientX;
-        let posRatonY = window.event.clientY;
-        console.log(posRatonX + "," + posRatonY);
-    },
-false);*/
+function ResetearHielo() {
+   
+    let bloques = Array.from(divBloque1.children);
+    bloques.forEach(element => {
+        element.style.width = '0px';
+        element.style.height = '0px';
+        divBloque1.removeChild(element);
+    });
+
+    divBloque1.classList.remove('agua');
+    divBloque1.classList.add('hielo');
+}
